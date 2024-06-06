1c678cf58efee8a5749d8843518bc495f0c61c6a
SylvioLabriola <sylvioez@gmail.com>
2024-06-06 18:01:15 -0300

    feat: protected the acess_key with some enviroment variables and changed the template names for my company name.

--- app/(website)/contact/contact.js
+++ app/(website)/contact/contact.js
@@ -24,48 +24,48 @@ export default function Contact({ settings }) {
   const [isSuccess, setIsSuccess] = useState(false);
   const [message, setMessage] = useState(false);
   // Please update the Access Key in the Sanity CMS - Site Congig Page
-  const apiKey = settings?.w3ckey || "YOUR_ACCESS_KEY_HERE";
-
+  const apiKey = settings?.w3ckey || process.env.NEXT_PUBLIC_W3_ACCESS_KEY;
+  
   const { submit: onSubmit } = useWeb3Forms({
     access_key: apiKey,
     settings: {
-      from_name: "Stablo Template",
-      subject: "New Contact Message from Stablo Website"
+      from_name: "Cougar's Car",
+      subject: "Novo LEAD gerado"
     },
     onSuccess: (msg, data) => {
       setIsSuccess(true);
-      setMessage(msg);
+      setMessage(msg);//Message that is displayed on the bottom of the button send.
       reset();
     },
     onError: (msg, data) => {
       setIsSuccess(false);
-      setMessage(msg);
+      setMessage(msg);//Message that is displayed on the bottom of the button send.
     }
   });
 
   return (
     <Container>
       <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
-        Contact
+        Contact us
       </h1>
       <div className="text-center">
-        <p className="text-lg">We are a here to help.</p>
+        <p className="text-lg">We are here to solve any of your doubts</p>
       </div>
 
       <div className="grid my-10 md:grid-cols-2">
         <div className="my-10">
           <h2 className="text-2xl font-semibold dark:text-white">
-            Contact Stablo
+            Contact Cougar's Car
           </h2>
           <p className="max-w-sm mt-5">
-            Have something to say? We are here to help. Fill up the
-            form or send email or call phone.
+            Have something to say or just want to get more info about something?
+            Leave your name, e-mail and whatsapp and will be getting in touch!
           </p>
 
           <div className="mt-5">
             <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-gray-400">
               <MapPinIcon className="w-4 h-4" />
-              <span>1734 Sanfransico, CA 93063</span>
+              <span>Rua das vendas de carro, 123, Rio de Janeiro - RJ</span>
             </div>
             {settings?.email && (
               <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-gray-400">
